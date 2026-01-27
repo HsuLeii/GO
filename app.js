0a751bf948c73d715cb10ad8eed02a9880a0d2ab
HsuLeii <ok841113@gmail.com>
2026-01-27 17:35:52 +0800

    0127

--- app.js
+++ app.js
@@ -112,7 +112,7 @@ cron.schedule(CONFIG.CHECK_INTERVAL, () => {
 
 // 定時傳送訊息
 setInterval(() => {
-    const statusMsg = `系統狀態：正常 (${ticketMessage}`;
+    const statusMsg = `系統狀態：正常 (${GREETING}`;
     io.emit('chat_message', statusMsg); 
 }, 10000);
 
