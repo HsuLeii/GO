3f64cc96cfb0a8052b08d6af1aba6193a08b71c9
HsuLeii <ok841113@gmail.com>
2026-02-03 16:35:46 +0800

    0203

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ async function checkTickets() {
         })
       })
   
-      // messageBody += `${ticketMessage}\n\n🔗 購票連結:\n${CONFIG.TKURL}`
+      messageBody += `${ticketMessage}\n\n🔗 購票連結:\n${CONFIG.TKURL}`
   
       // 4. 發送 LINE Push Message
       // console.log(messageBody)
